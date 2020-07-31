2d76430db3804e1474ec301ac10eaa07c0df7375
fnascimento <felipe.basina@gmail.com>
2020-07-31 19:30:15 -0300

    Adicionado operacao para remover comentarios.

--- simple-form/src/Form.js
+++ simple-form/src/Form.js
@@ -3,7 +3,24 @@ import React, { Component } from "react";
 class Form extends Component {
   constructor(props) {
     super(props);
-    this.state = { comments: [] };
+    this.state = { comments: [], enableButton: false, };
+  }
+
+  deleteAll() {
+    const requestOptions = {
+        method: "DELETE",
+      };
+      fetch("http://localhost:3001/comments", requestOptions)
+        .then((response) => response.json())
+        .then(
+          (result) => {
+            console.log(result);
+          },
+          (error) => {
+            console.log(`Error ${error}`);
+          }
+        );
+      this.handleChange();
   }
 
   save() {
@@ -23,7 +40,7 @@ class Form extends Component {
         }
       );
     this.newText.value = "";
-    this.componentDidMount();
+    this.handleChange();
   }
 
   getComments() {
@@ -53,13 +70,23 @@ class Form extends Component {
     this.getComments();
   }
 
+  handleChange() {
+      const comment = this.newText.value;
+      if (comment !== '') {
+        this.setState({enableButton: true});
+      } else {
+        this.setState({enableButton: false});
+      } 
+  }
+
   render() {
     function showValue(id) {
       fetch(`http://localhost:3001/comments/${id}`)
         .then((res) => res.json())
         .then(
           (result) => {
-            console.log(result);
+            const comment = JSON.stringify(result);
+            console.log(comment);
           },
           (error) => {
             console.log(`Error ${error}`);
@@ -69,13 +96,15 @@ class Form extends Component {
 
     return (
       <div className="comment">
-        <h1> Comentários</h1>
-        <textarea
+        <h1> Comentário</h1>
+        <textarea className="textArea"
           ref={(comment) => {
             this.newText = comment;
           }}
+          onChange={this.handleChange.bind(this)}
         />
-        <button onClick={this.save.bind(this)}>Save</button>
+        <button onClick={this.save.bind(this)} disabled={!this.state.enableButton}>Cadastrar</button>
+        <button onClick={this.deleteAll.bind(this)}>Remover comentários</button>
         <ul>
           {this.state.comments.map(function (comment, index) {
             return (
