71c2bcc44ad77e486812419469f570f0b229e85c
fnascimento <felipe.basina@gmail.com>
2020-09-12 12:25:55 -0300

    Adicionado funcao handler para botao.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -4,14 +4,27 @@ import Person from './Person/Person';
 
 class App extends Component {
 
+    state = {
+        persons: [
+            { name: "Name 1", age: 28 },
+            { name: "Name 2", age: 25 },
+            { name: "Name 3", age: 22 },
+        ],
+    }
+
+    switchNameHandler = () => {
+        //console.log('Was clicked!');
+        this.state.persons[0].name = "Full Name";
+    }
+
     render () {
         return (
             <div className="App">
                 <h1>Hi, I'm a React App.</h1>
-                <button>Switch Name</button>
-                <Person name="Name 1" age="20" />
-                <Person name="Name 2" age="23" />
-                <Person name="Name 3" age="25">My Hobbies: Racing</Person>
+                <button onClick={this.switchNameHandler}>Switch Name</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>My Hobbies: Racing</Person>
             </div>
         );
     }
