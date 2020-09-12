68bbb1affcf6efcc2a86a023ceb4a014119624e8
fnascimento <felipe.basina@gmail.com>
2020-09-12 12:12:47 -0300

    Alterado componente principal para ser classe.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-        <h1>Hi, I'm a React App.</h1>
-        <Person name="Name 1" age="20" />
-        <Person name="Name 2" age="23" />
-        <Person name="Name 3" age="25">My Hobbies: Racing</Person>
-    </div>
-  );
+class App extends Component {
+
+    render () {
+        return (
+            <div className="App">
+                <h1>Hi, I'm a React App.</h1>
+                <button>Switch Name</button>
+                <Person name="Name 1" age="20" />
+                <Person name="Name 2" age="23" />
+                <Person name="Name 3" age="25">My Hobbies: Racing</Person>
+            </div>
+        );
+    }
+
 }
 
 export default App;
