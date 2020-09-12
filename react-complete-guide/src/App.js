73b6e111a0014bae2bed64383a26850042174360
fnascimento <felipe.basina@gmail.com>
2020-09-12 12:08:00 -0300

    Adicionado componente Person e alterado componente principal para utiliza-lo.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi, I'm a React App.</h1>
+        <h1>Hi, I'm a React App.</h1>
+        <Person name="Name 1" age="20" />
+        <Person name="Name 2" age="23" />
+        <Person name="Name 3" age="25">My Hobbies: Racing</Person>
     </div>
   );
 }
