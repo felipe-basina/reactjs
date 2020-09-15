ff0c7c0ccc5c4f8c53a8abacb23a01bd0fc34219
fnascimento <felipe.basina@gmail.com>
2020-09-14 21:15:59 -0300

    Alterado estrutura para exibicao de conteudo de array atraves da funcao js map.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -55,17 +55,15 @@ class App extends Component {
         if (this.state.showPersons) {
             persons = (
                 <div>
-                    <Person
-                        name={this.state.persons[0].name}
-                        age={this.state.persons[0].age}/>
-                    <Person
-                        name={this.state.persons[1].name}
-                        age={this.state.persons[1].age}
-                        changed={this.nameChangeHandler}/>
-                    <Person
-                        name={this.state.persons[2].name}
-                        age={this.state.persons[2].age}
-                        click={this.switchNameHandler.bind(this, 'New name')}>My Hobbies: Racing</Person>
+                    {
+                        this.state.persons.map(person => {
+                            return (
+                                <Person
+                                    name={person.name}
+                                    age={person.age} />
+                            );
+                        })
+                    }
                 </div>
             );
         }
