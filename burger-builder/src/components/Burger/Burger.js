2042f0f98a2a72969471811a3ea8666b18501469
fnascimento <felipe.basina@gmail.com>
2020-10-19 23:14:15 -0300

    Ajustes para permtir exibicao de mensagem quando nao existirem ingredientes.

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -4,7 +4,7 @@ import classes from './Burger.css';
 import BurgerIngredient from "./BurgerIngredients/BurgerIngredients";
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             console.log(igKey
                 + ' ' + props.ingredients[igKey]
@@ -13,8 +13,15 @@ const burger = (props) => {
                 .map((_, index) => { // Ignora o valor, ficando apenas com o índice
                     return <BurgerIngredient key={igKey + index} type={igKey} />;
                 });
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el);
+        }, []); // [] valor inicial = array vazio
     console.log(transformedIngredients);
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>;
+    }
 
     return (
       <div className={classes.Burger}>
