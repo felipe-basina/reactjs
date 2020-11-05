c394149ce140eeb8895d5ac62daa0cff1dcdb820
fnascimento <felipe.basina@gmail.com>
2020-11-04 21:31:44 -0300

    Adicionado o componente controlador no builder.

--- burger-builder/src/components/Burger/BuildControls/BuildControls.js
+++ burger-builder/src/components/Burger/BuildControls/BuildControls.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
-import BuildControls from 'BuildControls.css';
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' },
+];
 
 const buildControls = (props) => (
-    <div className={BuildControls}></div>
+    <div className={classes.BuildControls}>
+        {controls.map(control => {
+            return <BuildControl key={control.label} label={control.type} />
+        })}
+    </div>
 );
 
 export default buildControls;
